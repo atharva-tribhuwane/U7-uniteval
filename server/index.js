44aeafdea8b13a80df035495f344689ba0ede53a
atharva-tribhuwane <tribhuwane.a@gmail.com>
2022-08-29 23:17:21 +0530

    adding video link

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const app = express();
 const {addUser, findUser} = require("./controller/user");
 const {connectDatabase} = require("./database/index");
-app.use(cors);
+// app.use(cors);
 app.use(express.json());
 
 
 app.post("/register", addUser);
 app.post("/login", findUser);
-
+// app.post("/getTodos", getTodos)
 
 connectDatabase().then(() => {
     app.listen(3001, () => {
