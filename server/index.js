1427c75868e822f4c300fc33a902ac2c3ecc82d0
atharva-tribhuwane <tribhuwane.a@gmail.com>
2022-08-29 22:34:43 +0530

    c

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const {addUser, findUser} = require("./controller/user");
 const {connectDatabase} = require("./database/index");
+app.use(cors);
 app.use(express.json());
+
 
 app.post("/register", addUser);
 app.post("/login", findUser);
